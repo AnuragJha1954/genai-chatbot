8706bd49ce479089e2187ce8a7728270b4f4a57c
AnuragJha1954 <56476154+AnuragJha1954@users.noreply.github.com>
2025-11-29 12:55:50 +0530

    Update sample2.py

--- sample2.py
+++ sample2.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # Only run this block for Gemini Developer API
-client = genai.Client(api_key='AIzaSyC2z6JrSJiz2CfbACivW4l8b07wm_Ou3ZY')
+client = genai.Client(api_key='')
 query = input("What do you want to ask?: ")
 # Stream the responses like on Gemini
 response = client.models.generate_content_stream(model="gemini-2.5-flash",contents=query)
