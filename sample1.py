170c3f977c6294c1a97c2f7545ba043572d7b565
AnuragJha1954 <56476154+AnuragJha1954@users.noreply.github.com>
2025-11-29 12:56:15 +0530

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # Only run this block for Gemini Developer API
-client = genai.Client(api_key='AIzaSyC2z6JrSJiz2CfbACivW4l8b07wm_Ou3ZY')
+client = genai.Client(api_key='')
 query = input("Enter your prompt here: ")
 # Send single prompt, wait for the model to return the entire output
 response = client.models.generate_content(model="gemini-2.5-flash", contents= query,)
